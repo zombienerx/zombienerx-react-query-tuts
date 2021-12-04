dc0c360c0cecf816c1da73dfa422dae777579bfb
zombienerx <nermin.sehic@gmail.com>
2021-12-04 21:41:21 +0100

    Added react-query to RQSuperHeroes to fetch data

--- rq-1/src/components/RQSuperHeroes.page.tsx
+++ rq-1/src/components/RQSuperHeroes.page.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import {useQuery} from 'react-query';
+import axios from 'axios'
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
 
 export const RQSuperHeroesPage = () => {
+  const {isLoading, data} = useQuery('super-heroes', fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading in progress...</h2>
+  }
+
   return (
     <>
-      <div>RQ Super Heroes Page</div>
+      <h2>RQ Super Heroes Page</h2>
+      {
+        data?.data.map((hero:any) => {
+          return <div key={hero.name}>{hero.name}</div>
+        })
+      }
     </>
   )
 }
