c62e4e3fb5851582c8dca0211804320a5e742412
zombienerx <nermin.sehic@gmail.com>
2021-12-05 11:51:17 +0100

    Added useQuery on click

--- rq-1/src/components/RQSuperHeroes.page.tsx
+++ rq-1/src/components/RQSuperHeroes.page.tsx
@@ -1,21 +1,31 @@
 import {useQuery} from 'react-query';
 import axios from 'axios'
 
+export default interface HeroData {
+  id?: any | null,
+  name: string,
+  alterEgo: string,
+}
+
 const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes')
 }
 
 export const RQSuperHeroesPage = () => {
   // const {isLoading, data, isError, error}
-  const {isLoading, data, isError, isFetching} = useQuery(
+  const {isLoading, data, isError, isFetching, refetch} = useQuery(
     'super-heroes',
     fetchSuperHeroes,
+    {
+      enabled: false
+    }
   );
+
+
 
   console.log(isLoading, isFetching);
 
-
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading in progress...</h2>
   }
 
@@ -27,6 +37,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={() => refetch()}>Fetch heroes</button>
       {
         data?.data.map((hero:any) => {
           return <div key={hero.name}>{hero.name}</div>
