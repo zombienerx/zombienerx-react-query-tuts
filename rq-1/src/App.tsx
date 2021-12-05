9d0b886a1c569f4ee1b626ebe3b829b51cb07deb
zombienerx <nermin.sehic@gmail.com>
2021-12-05 19:13:23 +0100

    Parallel Queries

--- rq-1/src/App.tsx
+++ rq-1/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import {QueryClientProvider, QueryClient} from 'react-query'
-import {ReactQueryDevtools} from 'react-query/devtools'
+import { QueryClientProvider, QueryClient} from 'react-query'
+import { ReactQueryDevtools} from 'react-query/devtools'
 import './App.css'
 import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
 import { SuperHeroesPage } from './components/SuperHeroes.page'
-import {RQSuperHeroPage} from './components/RQSuperHero.page'
+import { RQSuperHeroPage } from './components/RQSuperHero.page'
+import { ParallelQueriesPage } from './components/ParallelQueries.page'
 const queryClient = new QueryClient();
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
             </ul>
           </nav>
           <Routes>
+            <Route path='/rq-parallel' element= {<ParallelQueriesPage />} />
             <Route path='/' element= {<HomePage />} />
             <Route path='/super-heroes' element={<SuperHeroesPage />} />
             <Route path='/rq-super-heroes' element ={<RQSuperHeroesPage />} />
