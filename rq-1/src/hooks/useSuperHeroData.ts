6e6d7a25e0fa6ea419ff620f663b747c9455749f
zombienerx <nermin.sehic@gmail.com>
2021-12-08 19:40:19 +0100

    Initial Query Data

--- rq-1/src/hooks/useSuperHeroData.ts
+++ rq-1/src/hooks/useSuperHeroData.ts
@@ -1,14 +1,21 @@
-import {useQuery} from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import axios from 'axios'
 
-const fetchSeuperHero = ({queryKey}: any) => {
-  const heroId = queryKey[1];
+const fetchSuperHero = ({ queryKey }: any) => {
+  const heroId = queryKey[1]
   return axios.get(`http://localhost:4000/superheroes/${heroId}`)
 }
 
 export const useSuperHeroData = (heroId: any) => {
-  return useQuery(
-    ['super-hero', heroId],
-    fetchSeuperHero
-  )
+  const queryClient = useQueryClient()
+  return useQuery(['super-hero', heroId], fetchSuperHero, {
+    initialData: () => {
+      const hero = queryClient.getQueryData<any>('super-heroes')?.data?.find((hero: any) => hero.id === parseInt(heroId))
+      if (hero) {
+        return { data: hero }
+      } else {
+        return undefined
+      }
+    }
+  })
 }
--- rq-1/src/hooks/useSuperHeroesData.ts
+++ rq-1/src/hooks/useSuperHeroesData.ts
@@ -1,4 +1,4 @@
-import {useQuery} from 'react-query';
+import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const fetchSuperHeroes = () => {
